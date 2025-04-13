fe4f3a4e0b3f335e407c4354a86c183780526372
Khong phai ANh <tianangthang7@gmail.com>
2025-04-13 13:25:41 +0700

    asd

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { NestExpressApplication } from "@nestjs/platform-express";
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: 'https://web.myassistant.vn',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: false,  // Changed to false when using '*' origin
     allowedHeaders: '*',
